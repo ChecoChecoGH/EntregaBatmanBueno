e3b84826db431e6e5659b4fb89a6c80679d24f6f
ChecoChecoGH <as.ignasi97@gmail.com>
2020-11-12 23:42:32 +0100

    Commit

--- app/src/main/java/com/example/entregabatman/MainActivity.kt
+++ app/src/main/java/com/example/entregabatman/MainActivity.kt
@@ -1,68 +1,90 @@
 package com.example.entregabatman
 
+import android.annotation.SuppressLint
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var b : Button
-    private lateinit var et : EditText
-    private lateinit var tv : TextView
-    private lateinit var lL : LinearLayout
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    private val focusListener = View.OnFocusChangeListener { vista, isFocused ->  if (isFocused) tv.text = vista.tag.toString()}
+        val b =  findViewById<Button>(R.id.b)
+        val et  =  findViewById<EditText>(R.id.et)
+        val im = findViewById<ImageView>(R.id.im)
+        val lL = findViewById<LinearLayout>(R.id.lL)
 
+        b.setOnClickListener {
+            if (et.visibility == View.INVISIBLE) {
+                et.visibility = View.VISIBLE
+                b.text = "Finalizar"
+            }else{
+                et.visibility = View.INVISIBLE
+            }
+        }
 
+        b.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                b.text = "Finalizar"
+            } else {
+                b.append("Comenzar")
+            }
 
-    private val textWatcher = object : TextWatcher{
+        }
+
+        et.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus) {
+                et.text.clear()
+            }else{
+                et.text.append("Introduce tu nombre: ")
+            }
+        }
+        et.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                tv.text="Introduce tu nombre"
+
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
             }
-
             override fun afterTextChanged(s: Editable?) {
-                b.isEnabled = et.text.isNotEmpty()
-                if(tv.text.isEmpty())
-                    tv.text="Introduce tu nombre"
-                else if (tv.text.equals("wayne"))
-                    lL.setBackgroundColor(Color.parseColor("#FFFFEB3B"))
-                else if (tv.text.equals("joker")) {
-                    tv.setTextColor(Color.parseColor("#FF5A1E1E"))
-                    lL.setBackgroundColor(Color.parseColor("#FFFFEB3B"))
+                s?.let {
+                    if(it.contains("Wayne")){
+                        im.visibility = View.VISIBLE
+                    }else
+                        if (it.contains("Joker")){
+                            b.setBackgroundColor(Color.parseColor(" #9b807a"))
+                            et.setBackgroundColor(Color.parseColor("#a2270c"))
+                            lL.setBackgroundColor(Color.parseColor("#421d16"))
+                        }
                 }
             }
-        }
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        b =  findViewById(R.id.b)
-        et = findViewById(R.id.et)
-        tv = findViewById(R.id.tv)
-        lL = findViewById(R.id.lL)
-
-        et.addTextChangedListener(textWatcher)
-        tv.addTextChangedListener(textWatcher)
-        b.setOnClickListener {
-            et.text.clear()
-        }
-
-        et.onFocusChangeListener = focusListener
-        tv.onFocusChangeListener = focusListener
-        b.onFocusChangeListener = focusListener
+        })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
